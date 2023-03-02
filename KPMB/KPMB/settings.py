ac2a610bc95b2b10f05a53014e049014fc9814d8
khair12345 <khairadam.harris@gmail.com>
2023-03-03 06:05:48 +0800

    first commit

--- KPMB/KPMB/settings.py
+++ KPMB/KPMB/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','yc9esijj+vb2%8+gthq&rf=&m8q$!u8td(%rwgn68hzl1rrnwt')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','v3%$s6pbry1ej!hqvrcl)mnqp%ym96znpg@84#8&nqamu!5i@#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG','')!='False'
+DEBUG = os.environ.get('DJANGO_DEBUG','') !='False'
 
 ALLOWED_HOSTS = []
 
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.White.NoiseMiddleWarew',
+    'whitenoise.'
 ]
 
 ROOT_URLCONF = 'KPMB.urls'
@@ -119,13 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR/ 'staticfiles'
+STATIC_ROOT=BASE_DIR / 'staticfiles'
+
 STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-db_from_env= dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
